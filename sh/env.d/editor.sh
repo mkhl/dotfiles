f29102b620a6e50d3ccdc3627b0039d8c6c26879
Martin Kuehl <martin.kuehl@gmail.com>
2009-01-05 17:58:07 +0100

    • Most commands expect `EDITOR` to return on closing. • Set `FCEDIT` to a waiting editor nonetheless.

--- sh/env.d/editor.sh
+++ sh/env.d/editor.sh
@@ -1,9 +1,9 @@
 # Default editor
 # I currently use TextMate:
-export EDITOR="mate"
+export EDITOR="mate_wait"
 export CVSEDITOR="mate_wait"
 export SVN_EDITOR="$CVSEDITOR"
 export GIT_EDITOR="$CVSEDITOR"
+export FCEDIT="$CVSEDITOR"
 export VISUAL="$EDITOR"
-export FCEDIT="$EDITOR"
 export TEXEDIT="$EDITOR --line %d %s"
