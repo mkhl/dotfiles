847872e64e594a62e2e33a7fe481f61ce78654b7
Martin Kuehl <martin.kuehl@gmail.com>
2009-03-31 15:46:09 +0200

    • Use Emacs.

--- sh/env.d/editor.sh
+++ sh/env.d/editor.sh
@@ -1,10 +1,11 @@
 # Default editor
-# I currently use TextMate:
-export EDITOR="mate_wait"
-export CVSEDITOR="mate_wait"
+# I'll use Emacs for a while, and fall back to TextMate:
+export ALTERNATE_EDITOR="mate_wait"
+export EDITOR="emacsclient"
+export CVSEDITOR="emacsclient"
 export SVN_EDITOR="$CVSEDITOR"
-export GIT_EDITOR="$CVSEDITOR -l1"
+# export GIT_EDITOR="$CVSEDITOR -l1"
 export FCEDIT="$CVSEDITOR"
 export VISUAL="$EDITOR"
-export TEXEDIT="$EDITOR --wait --line %d '%s'"
-export LESSEDIT="$EDITOR --line %lm %f"
+# export TEXEDIT="$EDITOR --wait --line %d '%s'"
+# export LESSEDIT="$EDITOR --line %lm %f"
