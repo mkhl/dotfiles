ae2cb1080bbb2e28d55c76833982670aa4dd72fa
Martin Kuehl <martin.kuehl@gmail.com>
2009-06-15 01:05:16 +0200

    Enable TextMate for editing.

--- sh/env.d/editor.sh
+++ sh/env.d/editor.sh
@@ -1,13 +1,13 @@
 # Default editor
-# # TextMate
-# export EDITOR="mate_wait"
-# export CVSEDITOR="mate_wait"
-# export SVN_EDITOR="$CVSEDITOR"
-# export GIT_EDITOR="$CVSEDITOR --line 1"
-# export FCEDIT="$CVSEDITOR"
-# export VISUAL="$EDITOR"
-# export TEXEDIT="$CVSEDITOR --line %d '%s'"
-# export LESSEDIT="$EDITOR --line %lm %f"
+# TextMate
+export EDITOR="mate_wait"
+export CVSEDITOR="mate_wait"
+export SVN_EDITOR="$CVSEDITOR"
+export GIT_EDITOR="$CVSEDITOR --line 1"
+export FCEDIT="$CVSEDITOR"
+export VISUAL="$EDITOR"
+export TEXEDIT="$CVSEDITOR --line %d '%s'"
+export LESSEDIT="$EDITOR --line %lm %f"
 # # Aquamacs
 # export EDITOR="emacsclient"
 # export ALTERNATE_EDITOR="aquamacs"
