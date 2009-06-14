89e1b8f7b180e9f9b96f6dc3cfbd0cf0acf2d614
Martin Kuehl <martin.kuehl@gmail.com>
2009-06-15 01:15:52 +0200

    Generalize pythonrc

--- repl/pythonrc.py
+++ repl/pythonrc.py
@@ -5,8 +5,16 @@ import os
 
 # Tab completion:
 import rlcompleter, readline
+
 # This works with GNU readline:
-# readline.parse_and_bind('tab: complete')
-# This works with BSD EditLine:
-readline.parse_and_bind('bind ^I rl_complete')
+def __use_readline():
+    readline.parse_and_bind('tab: complete')
+
+# This works with BSD editline:
+def __use_editline():
+    readline.parse_and_bind('bind ^I rl_complete')
+
 # Note that OS X.5 (Leopard) uses the latter!
+# Note also that editline happily ignores the former definition,
+#  while readline silently corrupts the REPL when parsing the latter.
+__use_readline()
