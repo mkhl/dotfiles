2e8d28a809d7fc6c8e0aedf6e79340172255f2db
Martin Kuehl <martin.kuehl@gmail.com>
2009-07-16 10:27:34 +0200

    Fix chicken scheme compiler flags for MacPorts.

--- sh/env.d/macports.sh
+++ sh/env.d/macports.sh
@@ -1,2 +1,4 @@
 # Fix libltdl search path for MacPorts
 export LTDL_LIBRARY_PATH="/opt/local/lib"
+# Fix chicken scheme compiler/linker paths
+export CSC_OPTIONS="-I/opt/local/include -L/opt/local/lib"
