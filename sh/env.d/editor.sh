e60affb08886ab9af24644a78f3e1186513feabe
Martin Kuehl <martin.kuehl@gmail.com>
2009-09-03 11:28:06 +0200

    Aaand back again…

--- sh/env.d/editor.sh
+++ sh/env.d/editor.sh
@@ -1,22 +1,22 @@
 # Default editor
 
-# # TextMate
-# export EDITOR="mate_wait"
-# export CVSEDITOR="mate_wait"
-# export SVN_EDITOR="$CVSEDITOR"
-# export GIT_EDITOR="$CVSEDITOR --line 1"
-# export FCEDIT="$CVSEDITOR"
-# export VISUAL="$EDITOR"
-# export TEXEDIT="$CVSEDITOR --line %d '%s'"
-# export LESSEDIT="$EDITOR --line %lm %f"
-
-# Emacs
-export EDITOR="emacsclient"
-export ALTERNATE_EDITOR="aquamacs"
-export CVSEDITOR="$EDITOR"
-export SVN_EDITOR="$EDITOR +1"
-export GIT_EDITOR="$EDITOR +1"
-export FCEDIT="$EDITOR"
+# TextMate
+export EDITOR="mate_wait"
+export CVSEDITOR="mate_wait"
+export SVN_EDITOR="$CVSEDITOR"
+export GIT_EDITOR="$CVSEDITOR --line 1"
+export FCEDIT="$CVSEDITOR"
 export VISUAL="$EDITOR"
-export TEXEDIT="$EDITOR +%d '%s'"
-export LESSEDIT="$EDITOR +%lm %f"
+export TEXEDIT="$CVSEDITOR --line %d '%s'"
+export LESSEDIT="$EDITOR --line %lm %f"
+
+# # Emacs
+# export EDITOR="emacsclient"
+# export ALTERNATE_EDITOR="aquamacs"
+# export CVSEDITOR="$EDITOR"
+# export SVN_EDITOR="$EDITOR +1"
+# export GIT_EDITOR="$EDITOR +1"
+# export FCEDIT="$EDITOR"
+# export VISUAL="$EDITOR"
+# export TEXEDIT="$EDITOR +%d '%s'"
+# export LESSEDIT="$EDITOR +%lm %f"
