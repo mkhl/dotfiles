4848c7aed2d5470c1b5cba50729e97f34b5b55ab
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-23 01:13:51 +0200

    Add emc alias

--- sh/alias.d/elisp.sh
+++ sh/alias.d/elisp.sh
@@ -1,4 +1,5 @@
 # Lisp byte-code compilers
+alias        emc='emacsclient --no-wait'
 alias   ecompile='emacs    -batch -f batch-byte-compile'
 alias  xecompile='xemacs   -batch -f batch-byte-compile'
 alias sxecompile='sxemacs  -batch -f batch-byte-compile'
