8bf54bffd3183436124d853e350cf33c83283208
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-17 18:39:04 +0100

    Make `emc' create new frames

--- sh/alias.d/elisp.sh
+++ sh/alias.d/elisp.sh
@@ -1,5 +1,5 @@
 # Lisp byte-code compilers
-alias        emc='emacsclient --no-wait'
+alias        emc='emacsclient --no-wait --create-frame'
 alias   ecompile='emacs    -batch -f batch-byte-compile'
 alias  xecompile='xemacs   -batch -f batch-byte-compile'
 alias sxecompile='sxemacs  -batch -f batch-byte-compile'
