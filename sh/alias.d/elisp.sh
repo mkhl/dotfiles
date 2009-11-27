9ad7d9f826bbc8d8b3eecbb500a4128fd81302a8
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-27 17:08:35 +0100

    Arguments reorder.

--- sh/alias.d/elisp.sh
+++ sh/alias.d/elisp.sh
@@ -1,5 +1,5 @@
 # Lisp byte-code compilers
-alias        emc='emacsclient --no-wait --create-frame'
+alias        emc='emacsclient --create-frame --no-wait'
 alias   ecompile='emacs    -batch -f batch-byte-compile'
 alias  xecompile='xemacs   -batch -f batch-byte-compile'
 alias sxecompile='sxemacs  -batch -f batch-byte-compile'
