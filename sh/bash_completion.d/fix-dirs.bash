856cf2b41beda04577d4a7bf15c4dd0c821335d1
Martin Kuehl <martin.kuehl@gmail.com>
2010-02-17 19:01:46 +0100

    Remove obsolete fix for bash v4 completions

--- sh/bash_completion.d/fix-dirs.bash
+++
@@ -1,5 +0,0 @@
-# Fix completion of dirs with spaces.
-if (( ${BASH_VERSINFO[0]} >= 4 )); then
-	compopt -o default $(complete -p| grep -e '-o filenames'| grep -v -e '-o default'| sed -E -e 's/.+\s+//')
-	compopt -o default cd
-fi
