d9d75dbf51e1751a8add324c569153511a89bbe3
Martin Kuehl <martin.kuehl@gmail.com>
2010-02-22 23:46:12 +0100

    Remove elisp aliases

--- sh/alias.d/elisp.sh
+++
@@ -1,7 +0,0 @@
-# Lisp byte-code compilers
-alias   ecompile='${EMACS:-emacs}     -batch -f batch-byte-compile'
-alias   ecompdir='${EMACS:-emacs}     -batch -f batch-byte-recompile-directory'
-alias  xecompile='${XEMACS:-xemacs}   -batch -f batch-byte-compile'
-alias  xecompdir='${XEMACS:-xemacs}   -batch -f batch-byte-recompile-directory'
-alias sxecompile='${SXEMACS:-sxemacs} -batch -f batch-byte-compile'
-alias sxecompdir='${SXEMACS:-sxemacs} -batch -f batch-byte-recompile-directory'
