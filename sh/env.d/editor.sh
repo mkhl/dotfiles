7b5ccfdb30520a591507ed30192745df21f102e5
Martin Kühl <martin.kuehl@gmail.com>
2010-04-14 15:29:35 +0200

    Shell: Use `asdfg` wherever I can.

--- sh/env.d/editor.sh
+++ sh/env.d/editor.sh
@@ -1,5 +1,8 @@
 # Default editor
 
+# asdfg
+export EDITOR="asdfg"
+
 # # TextMate
 # export EDITOR="mate_wait"
 # export CVSEDITOR="mate_wait"
@@ -10,13 +13,13 @@
 # export TEXEDIT="$CVSEDITOR --line %d '%s'"
 # export LESSEDIT="$EDITOR --line %lm %f"
 
-# Emacs
-export EDITOR="emacsclient"
-export ALTERNATE_EDITOR="mg"
-export CVSEDITOR="$EDITOR"
-export SVN_EDITOR="$EDITOR +1"
-export GIT_EDITOR="$EDITOR +1"
-export FCEDIT="$EDITOR"
-export VISUAL="$EDITOR"
-export TEXEDIT="$EDITOR +%d '%s'"
-export LESSEDIT="$EDITOR +%lm %f"
+# # Emacs
+# export EDITOR="emacsclient"
+# export ALTERNATE_EDITOR="mg"
+# export CVSEDITOR="$EDITOR"
+# export SVN_EDITOR="$EDITOR +1"
+# export GIT_EDITOR="$EDITOR +1"
+# export FCEDIT="$EDITOR"
+# export VISUAL="$EDITOR"
+# export TEXEDIT="$EDITOR +%d '%s'"
+# export LESSEDIT="$EDITOR +%lm %f"
