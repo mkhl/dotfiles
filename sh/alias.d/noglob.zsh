634943a20adda35c2b7f80ef1668ea04b3bed512
Martin Kühl <martin.kuehl@gmail.com>
2010-05-25 20:02:07 +0200

    Shell: Get rid of obsolete fastri references.

--- sh/alias.d/noglob.zsh
+++ sh/alias.d/noglob.zsh
@@ -1,6 +1,4 @@
 # Prevent globbing
 alias ri='noglob ri'
-alias fri='noglob fri'
-alias qri='noglob qri'
 alias openri='noglob openri'
 alias curl='noglob curl'
--- sh/alias.d/ri.sh
+++
@@ -1,4 +0,0 @@
-# Prefer qri if it's available
-if has_cmd qri; then
-    alias ri="paged qri"
-fi
