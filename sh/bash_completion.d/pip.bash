a1387d13121a7cb0df1003d0d622a4140f8c184d
Martin Kühl <martin.kuehl@gmail.com>
2010-07-08 22:23:02 +0200

    Shell: Load pip(1) completions lazily.

--- sh/bash_completion.d/pip.bash
+++ sh/bash_completion.d/pip.bash
@@ -1,4 +1,8 @@
 # Pip completion
-if has_cmd pip; then
-    eval "$(pip completion --$is)"
-fi
+_pip_completion() {
+	if has_cmd pip; then
+		eval "$(pip completion --$is)"
+	fi
+	_pip_completion "$@"
+}
+complete -o default -F _pip_completion pip
