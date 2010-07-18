ed1f702eec7181186fc83ca5b3b3c23f94c8badd
Martin Kühl <martin.kuehl@gmail.com>
2010-07-18 05:41:40 +0200

    Shell: Tweak Plan9 fonts.

--- sh/env.d/plan9.sh
+++ sh/env.d/plan9.sh
@@ -1,4 +1,4 @@
 # Plan 9
 export PLAN9=/opt/plan9port
-export font=$PLAN9/font/fixed/unicode.8x13.font
-# export font=/mnt/font/TerminusMedium/14/font
+export font=$PLAN9/font/fixed/unicode.7x13.font
+# export font=/mnt/font/Monaco/10/font
