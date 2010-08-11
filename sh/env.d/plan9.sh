51cde697b6fddd80d464cc7d6a7c0946acf4be66
Martin Kühl <martin.kuehl@gmail.com>
2010-08-11 03:48:58 +0200

    Shell: Smaller Plan9 font.

--- sh/env.d/plan9.sh
+++ sh/env.d/plan9.sh
@@ -1,5 +1,5 @@
 # Plan 9
 export PLAN9=/opt/plan9port
-export font=$PLAN9/font/fixed/unicode.7x13.font
+export font=$PLAN9/font/fixed/unicode.6x13.font
 # export font=/mnt/font/Monaco/10/font
 # export font=/mnt/font/TerminusMedium/12/font
