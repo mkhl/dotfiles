449ad458cd16747dadd1f8a924a73e18b40022ad
Martin Kühl <martin.kuehl@gmail.com>
2010-09-15 14:27:27 +0200

    Repl: Disable autoloading of `asdf-install`.

--- repl/ccl-init.lisp
+++ repl/ccl-init.lisp
@@ -3,11 +3,4 @@
 #-asdf
 (require :asdf)
 
-#-asdf-install
-(progn
-  (pushnew #P"ccl:tools;asdf-install;"
-           asdf:*central-registry*
-           :test #'equal)
-  (asdf:oos 'asdf:load-op :asdf-install))
-
 (load (merge-pathnames ".common.lisp" (user-homedir-pathname)))
