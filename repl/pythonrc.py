f731fb6f7b991734167438fbd5f8250c2df99b7f
Martin Kühl <martin.kuehl@gmail.com>
2011-01-23 00:31:08 +0100

    Repl: python(1): Update configuration.
    * Simplify tab completion initialization.
    * Import pprint module.

--- repl/pythonrc.py
+++ repl/pythonrc.py
@@ -1,20 +1,17 @@
 #  -*- mode: python -*-
+# python(1)
 
-# Load a few useful modules:
-import os
+# Default modules
+import os, sys
 
-# Tab completion:
-import rlcompleter, readline
+# Pretty-printing
+from pprint import pprint as pp
 
-# This works with GNU readline:
-def __use_readline():
-    readline.parse_and_bind('tab: complete')
-
-# This works with BSD editline:
-def __use_editline():
-    readline.parse_and_bind('bind ^I rl_complete')
-
-# Note that OS X.5 (Leopard) uses the latter!
-# Note also that editline happily ignores the former definition,
-#  while readline silently corrupts the REPL when parsing the latter.
-__use_readline()
+# Line editing
+try:
+    import readline, rlcompleter
+    readline.parse_and_bind('tab: complete')        # GNU readline
+    # readline.parse_and_bind('bind ^I rl_complete')  # BSD editline
+    del readline, rlcompleter
+except ImportError:
+    pass
